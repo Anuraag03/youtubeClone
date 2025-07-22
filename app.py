2c4cc8544c83a33b3f65ff060a026e5d6acad59d
Anuraag03 <f20211887@hyderabad.bits-pilani.ac.in>
2025-07-22 15:17:43 +0530

    Changes to file structure

--- app.py
+++ app.py
@@ -23,4 +23,6 @@ def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))  # 8000 is fallback for local dev
+    app.run(host='0.0.0.0', port=port)
+
